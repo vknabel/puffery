7f164a4840802cc76372c9e5d4736dd4e4fe9609
Valentin Knabel <dev@vknabel.com>
2020-04-13 17:22:50 +0200

    Initial Commit

---
+++ Wake/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Wake
+//
+//  Created by Valentin Knabel on 13.04.20.
+//  Copyright © 2020 Valentin Knabel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WakeTests/WakeTests.swift
@@ -0,0 +1,34 @@
+//
+//  WakeTests.swift
+//  WakeTests
+//
+//  Created by Valentin Knabel on 13.04.20.
+//  Copyright © 2020 Valentin Knabel. All rights reserved.
+//
+
+import XCTest
+@testable import Wake
+
+class WakeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
