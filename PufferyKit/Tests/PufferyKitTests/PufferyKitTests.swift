32700123d6f4937d1b25415cecc279956806a727
Valentin Knabel <dev@vknabel.com>
2020-07-03 22:35:23 +0200

    Added if can imports and mocks

--- PufferyKit/Tests/PufferyKitTests/PufferyKitTests.swift
+++ PufferyKit/Tests/PufferyKitTests/PufferyKitTests.swift
@@ -6,7 +6,6 @@ final class PufferyKitTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(PufferyKit().text, "Hello, World!")
     }
 
     static var allTests = [
