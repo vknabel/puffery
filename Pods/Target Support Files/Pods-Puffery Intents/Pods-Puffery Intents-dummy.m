870b5bd04ce16eebfbb8579e0368a7903e8c63c5
Valentin Knabel <dev@vknabel.com>
2020-08-14 23:36:31 +0200

    Removed CocoaPods

--- Pods/Target Support Files/Pods-Puffery Intents/Pods-Puffery Intents-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Puffery_Intents : NSObject
-@end
-@implementation PodsDummy_Pods_Puffery_Intents
-@end
--- Pods/Target Support Files/Pods-Puffery-PufferyUITests/Pods-Puffery-PufferyUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Puffery_PufferyUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Puffery_PufferyUITests
-@end
