3fc69a0c7d134c7ef7bb0ef172743b7ccc7271ef
Valentin Knabel <dev@vknabel.com>
2021-06-20 23:28:43 +0200

    Update AppDelegate.swift

--- Puffery/AppDelegate.swift
+++ Puffery/AppDelegate.swift
@@ -45,7 +45,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
         Widgets.reloadAll()
-        completionHandler(.noData)
+        completionHandler(.newData)
     }
 
     // MARK: - Core Data stack
